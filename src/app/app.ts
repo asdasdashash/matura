0622ab900925da3e28c836ac76bec4ab15b95db0
asdasdashash <tin.zinko1@gmail.com>
2025-11-28 04:16:21 +0100

    Update project to latest version

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';  // ← import this
+import { ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,14 @@ import { CommonModule } from '@angular/common';  // ← import this
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
+
 export class App {
   protected readonly title = signal('matura');
 
+  @ViewChildren('cell') cells!: QueryList<ElementRef>;
+
+  chance: number = 0;
+  mineId: number = 0;
   mineNum: number = 5;
   mineMenuOpen = false;
 
@@ -21,8 +27,31 @@ export class App {
   }
 
   confirmMines() {
-    console.log('Mines set to:', this.mineNum);
-    this.mineMenuOpen = false;
-  }
-}
+    if(this.mineNum > 24 || this.mineNum < 1){
+      console.log('debil sii')
+      
+    }
 
+    else{
+      console.log('Mines set to:', this.mineNum);
+      this.mineMenuOpen = false;
+      this.chance = (this.mineNum/25)*100
+
+      console.log(this.chance)
+    }
+
+  }
+
+  getMineId(id: number){
+    this.mineId = id;
+    console.log(this.mineId);
+
+    const cellArray = this.cells.toArray();  // make normal array
+
+    const cell = cellArray[id - 1].nativeElement; // because your IDs start at 1
+
+    cell.style.backgroundColor = 'yellow';
+
+  }
+
+}
