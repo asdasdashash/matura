f0518f2cf4cbbec5c17d151c1401675659800806
asdasdashash <tin.zinko1@gmail.com>
2025-11-28 08:31:51 +0100

    Initial upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';  // ← import this
+  // ← import this
 import { ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],  // ← add CommonModule here
+  imports: [RouterOutlet, FormsModule],  // ← add CommonModule here
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
@@ -16,12 +16,14 @@ export class App {
   protected readonly title = signal('matura');
 
   @ViewChildren('cell') cells!: QueryList<ElementRef>;
+  denar: number = 100;
+
   randomNumber:number = 0;
   chance: number = 0;
   mineId: number = 0;
   mineNum: number = 5;
-  mineMenuOpen = false;
-
+  mineMenuOpen = true;
+  safePolja = 0;
   openMineMenu() {
     this.mineMenuOpen = true;
   }
@@ -34,31 +36,39 @@ export class App {
 
     else{
       console.log('Mines set to:', this.mineNum);
-      this.mineMenuOpen = false;
-      this.chance = (this.mineNum/25)*100
+      this.safePolja = 25;
 
-      console.log(this.chance)
+      this.mineMenuOpen = false;
+      this.chance = (this.mineNum/25)*100;
+      this.safePolja = this.safePolja - this.mineNum;
+      console.log(this.safePolja);
     }
 
   }
 
   getMineId(id: number){
     this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(this.randomNumber)
+    console.log("šansa za mino "+this.chance)
+    console.log("st varnih polj "+this.safePolja)
 
 
     this.mineId = id;
-    console.log(this.mineId);
 
     const cellArray = this.cells.toArray();  // make normal array
 
     const cell = cellArray[id - 1].nativeElement; // because your IDs start at 1
 
-    if(this.chance<this.randomNumber){
+    if(this.chance < this.randomNumber && this.safePolja > 0){
       cell.style.backgroundColor = "yellow";
     }
 
+    else{
+      cell.style.backgroundColor = "red";
+    }
 
 
+    this.safePolja--;
   }
 
 }
